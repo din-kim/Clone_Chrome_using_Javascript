97eaa80c1aa644dc85fe95f2ff684154abea6485
din-kim <122077509+din-kim@users.noreply.github.com>
2023-01-25 19:15:45 +0900

    Display weather metrics and more wallpapers

--- js/wallpaper.js
+++ js/wallpaper.js
@@ -1,4 +1,9 @@
-const wallpapers = ["starwars-4.jpg","starwars-5.jpg",]
+const wallpapers = [
+    "starwars-1.jpg", "starwars-2.jpg", "starwars-3.jpg", "starwars-4.jpg", "starwars-5.jpg",
+    "starwars-6.jpg", "starwars-7.jpg", "starwars-8.jpg", "starwars-9.jpg", "starwars-10.jpg",
+    "starwars-11.jpg", "starwars-12.jpg", "starwars-13.jpg", "starwars-14.jpg", "starwars-15.jpg",
+    "starwars-16.jpg", "starwars-17.jpg", "starwars-18.jpg", "starwars-19.jpg", "starwars-20.jpg",
+]
 const chosenPicture = wallpapers[Math.floor(Math.random() * wallpapers.length)];
 const todaysWallpaper = document.createElement("img");
 todaysWallpaper.src = `assets/img/${chosenPicture}`;
--- js/weather.js
+++ js/weather.js
@@ -3,13 +3,15 @@ const API_KEY = "961324830d532de52b2432e36bef6afd";
 function onGeoSuccess(position) {
     const lat = position.coords.latitude;
     const long = position.coords.longitude;
-    const currentWeatherUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`;
+    const currentWeatherUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&appid=${API_KEY}&units=metric`;
     fetch(currentWeatherUrl)
         .then((response) => response.json())
         .then((data) => {
             const weather = document.querySelector("#weather span:first-child");
+            const temp = document.querySelector("#weather span:nth-child(2)");
             const cityName = document.querySelector("#weather span:last-child");
             weather.innerText = data.weather[0].main;
+            temp.innerText = `${Math.round(data.main.temp,2)}`+'\xB0C';
             cityName.innerText = data.name;            
         });
 }
